c5f7bbb87bf2883fc031a88bdca3c26c9759df96
lorennam <lorennamenesesdealmeida@gmail.com>
2026-05-03 00:44:32 -0300

    fazendo navbar

---
+++ frontend/app/components/Navbar/navbar.js
@@ -0,0 +1,37 @@
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { LiaUserSolid } from "react-icons/lia";
+import { IoIosSearch } from "react-icons/io";
+import { BsHandbag } from "react-icons/bs";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.navbarInterna}>
+          <div className={styles.esquerda}>
+            <IoIosSearch className={styles.imgNav} size={34}/>
+          </div>
+          <div className={styles.centro}>
+            <Link href="/home" className={styles.linkNav}>HOME</Link>
+            <Link href="/produtos" className={styles.linkNav}>PRODUTOS</Link>
+            <Link href="/agendar-exames" className={styles.linkNav}>AGENDAR EXAMES</Link>
+            <Link href="/contato" className={styles.linkNav}>CONTATO</Link>
+          </div>
+          <div className={styles.direita}>
+            <Link href="/pesquisa" className={styles.linkNav}>
+              <IoIosSearch className={styles.imgNav} size={34} />
+            </Link>
+            <Link href="/carrinho" className={styles.linkNav}>
+              <BsHandbag className={styles.imgNav} size={30} />
+            </Link>
+            <Link href="/perfil" className={styles.linkNav}>
+              <LiaUserSolid className={styles.imgNav} size={39} />
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
