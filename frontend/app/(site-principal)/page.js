b264009a0b1c74523cf693b9570c0c143545d1fb
Gabriella <gabriellatlr24@gmail.com>
2026-05-13 23:40:37 -0300

    begin home

--- frontend/app/(site-principal)/page.js
+++ frontend/app/(site-principal)/page.js
@@ -1,12 +1,23 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <>
-    <div>
-      <h1>home</h1>
-    </div>
-    </>
+    <main className={styles.main}>
+      <section className={styles.hero}>
+        <nav className={styles.navbar}>
+          <div className={styles.logo}>
+          </div>
+        </nav>
+
+        <div className={styles.heroContent}>
+          <h1>
+            ENCONTRE <br />
+            <span>O ÓCULOS</span> <br />
+            <strong className={styles.highlight}>PERFEITO</strong> <br />
+            para <span className={styles.underline}>VOCÊ!</span>
+          </h1>
+        </div>
+      </section>
+    </main>
   );
 }
