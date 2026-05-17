696897aa74a3f4b596b721621ddfdbf48862925a
joao0cb <joao.00000853774@unicap.br>
2026-05-16 21:11:05 -0300

    _

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
-from .models import Oculos, Reserva, ExameAgendamento
+from .models import Oculos, OculosVarianteCor, OculosImagem, Reserva, ExameAgendamento
 
-admin.site.register(Oculos)
 
-# reservas/admin.py
+class OculosImagemInline(admin.TabularInline):
+    model = OculosImagem
+    extra = 1
+
+
+class OculosVarianteInline(admin.TabularInline):
+    model = OculosVarianteCor
+    extra = 1
+    show_change_link = True
+
+
+@admin.register(Oculos)
+class OculosAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug', 'criado_em']
+    inlines = [OculosVarianteInline]
+
+
+@admin.register(OculosVarianteCor)
+class OculosVarianteCorAdmin(admin.ModelAdmin):
+    inlines = [OculosImagemInline]
+    list_display = ['oculos', 'cor', 'quantidade_estoque', 'preco_adicional']
+
+
 @admin.register(Reserva)
 class ReservaAdmin(admin.ModelAdmin):
     list_display = ['id', 'nome_cliente', 'telefone_whatsapp', 'oculos', 'status', 'criado_em']
@@ -11,6 +32,26 @@ class ReservaAdmin(admin.ModelAdmin):
     search_fields = ['nome_cliente', 'telefone_whatsapp', 'usuario__username']
     readonly_fields = ['oculos_snapshot', 'criado_em', 'atualizado_em']
     list_editable = ['status']
+
+    def save_model(self, request, obj, form, change):
+        if not obj.usuario_id:
+            obj.usuario = request.user
+        if not obj.oculos_snapshot and obj.oculos:
+            oculos = obj.oculos
+            obj.oculos_snapshot = {
+                'id': oculos.id,
+                'codigo_referencia': oculos.codigo_referencia,
+                'nome': oculos.nome,
+                'marca': oculos.get_marca_display(),
+                'material': oculos.get_material_display(),
+                'formato': oculos.get_formato_display(),
+                'genero': oculos.get_genero_display(),
+                'medida_aro': oculos.medida_aro,
+                'medida_ponte': oculos.medida_ponte,
+                'medida_haste': oculos.medida_haste,
+                'preco': str(oculos.preco),
+            }
+        super().save_model(request, obj, form, change)
 
     fieldsets = (
         ('Cliente', {
@@ -27,13 +68,14 @@ class ReservaAdmin(admin.ModelAdmin):
         }),
     )
 
+
 @admin.register(ExameAgendamento)
 class ExameAgendamentoAdmin(admin.ModelAdmin):
     list_display = ['id', 'nome_cliente', 'telefone_whatsapp', 'data_preferencia',
                     'periodo_preferido', 'status', 'criado_em']
     list_filter = ['status', 'periodo_preferido']
     search_fields = ['nome_cliente', 'telefone_whatsapp', 'usuario__username']
-    readonly_fields = ['criado_em', 'atualizado_em']
+    readonly_fields = ['criado_em', 'atualizado_em', 'usuario']
     list_editable = ['status']
 
     fieldsets = (
