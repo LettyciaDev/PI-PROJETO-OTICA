cf29670bb75bd52cca8f0198190370e7f14336da
Bento Guilherme <gbento555@gmail.com>
2026-06-02 18:19:51 -0300

    static no settings

--- backend/settings.py
+++ backend/settings.py
@@ -161,3 +161,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
